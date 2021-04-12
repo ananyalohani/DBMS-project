2c4978038a465cca3ed0d00d4679ad40b9b48064
Udit Bhati <55918713+udit19281@users.noreply.github.com>
2021-04-13 00:05:25 +0530

    CLI Added

---
+++ CLI/spectrumdb.py
@@ -0,0 +1,142 @@
+import pyfiglet
+import mysql.connector
+
+def authors():
+    pass
+
+def volunteers():
+    pass
+
+def organizers():
+    pass
+
+def tutors():
+    pass
+
+def attendees():
+    pass
+
+def workshop_organizers():
+    pass
+
+def stats():
+    choice=0
+    while choice!=5:
+        print(" 1) Top Rankings of authors based on number of papers \n 2) Top Rankings of institutions based on number of papers in MainTrack\n 3) Papers which won Awards \n 4) Summary statistics (Total authors, papers) \n 5) Main Menu \n")
+        choice=int(input("Select Query:"))
+        if choice==1:
+            query1a="select AuthorID, count(*) as MainTrackPapers from MainTrackAuthors group by AuthorID order by MainTrackPapers desc;"
+            cursor.execute(query1a)
+            print("\n[+] Main Track paper publised by Authors [+] \n")
+            j=1
+            for (a,b) in cursor.fetchall():
+                print("{}. AuthorID: {},Total Papers: {}".format(j,a,b))
+                j+=1
+
+            query1b="select AuthorID, count(*) as WorkshopPapers from WkshpPaperAuthors group by AuthorID order by WorkshopPapers desc;"
+            cursor.execute(query1b)
+            j=1
+            print("\n[+] Workshop papers publised by Authors [+] \n")
+            for (a,b) in cursor.fetchall():
+                print("{}. AuthorID: {},Total Papers: {}".format(j,a,b))
+                j+=1
+
+            query1c="select AuthorID,count(AuthorID) as TotalPapers from ((select AuthorID from WkshpPaperAuthors) union all (select AuthorID from MainTrackAuthors) ) as t group by AuthorID order by TotalPapers desc; "
+            cursor.execute(query1c)
+            j=1
+            print("\n[+] Workshop and Main Track paper publised by Authors [+] \n")
+            for (a,b) in cursor.fetchall():
+                print("{}. AuthorID: {},Total Papers: {}".format(j,a,b))
+                j+=1
+            print()
+        if choice==2:
+            query1a="select Affiliation as Institution, count(*) as MainTrackPapers from (select distinct Affiliation, PaperID from MainTrackAuthors, Authors where MainTrackAuthors.AuthorID = Authors.AuthorID) as t group by Institution order by MainTrackPapers desc;"
+            cursor.execute(query1a)
+            print("\n[+] Main Track paper publised by Institution [+] \n")
+            j=1
+            for (a,b) in cursor.fetchall():
+                print("{}. Institution: {},  Total Papers: {}".format(j,a,b))
+                j+=1
+
+            query1b="select Affiliation as Institution, count(*) as WorkshopPapers from (select distinct Affiliation, WPaperID from WkshpPaperAuthors, Authors where WkshpPaperAuthors.AuthorID = Authors.AuthorID) as t group by Institution order by WorkshopPapers desc;"
+            cursor.execute(query1b)
+            j=1
+            print("\n[+] Workshop papers publised by Institution [+] \n")
+            for (a,b) in cursor.fetchall():
+                print("{}. Institution: {},  Total Papers: {}".format(j,a,b))
+                j+=1
+
+            query1c="select Affiliation as Institution, count(*) as PapersPublished from ((select distinct Affiliation, PaperID from MainTrackAuthors, Authors where MainTrackAuthors.AuthorID = Authors.AuthorID) union all (select distinct Affiliation, WPaperID from WkshpPaperAuthors, Authors where WkshpPaperAuthors.AuthorID = Authors.AuthorID)) as t group by Institution order by PapersPublished desc;"
+            cursor.execute(query1c)
+            j=1
+            print("\n[+] Workshop and Main Track paper publised by Institution [+] \n")
+            for (a,b) in cursor.fetchall():
+                print("{}. Institution: {},  Total Papers: {}".format(j,a,b))
+                j+=1
+            print()
+        if choice==3:
+            query1a="select title from maintrackpapers where PaperID in (select PaperID from awards where PaperID is not null); "
+            cursor.execute(query1a)
+            print("\n[+] Main Track paper [+] \n")
+            j=1
+            for (a) in cursor.fetchall():
+                print("{}. Title: {}".format(j,a))
+                j+=1
+
+            query1b="select title from workshoppapers where WPaperID in (select WPaperID from awards where WPaperID is not null);"
+            cursor.execute(query1b)
+            j=1
+            print("\n[+] Workshop papers [+] \n")
+            for (a) in cursor.fetchall():
+                print("{}. Title: {}".format(j,a))
+                j+=1
+            print()
+        if choice==4:
+            query1a="select * from (select count(*) as TotalAuthors from Authors) as t1, (select count(*) as TotalWorkshops from Workshops) as t2, (select count(*) as MainTrackPaperCount from MainTrackPapers) as t3, (select count(*) as WorkshopPaperCount from WorkshopPapers)  as SummaryStatistics;"
+            cursor.execute(query1a)
+            for (a,b,c,d) in cursor.fetchall():
+                print("Total Authors:{}, Total Workshops:{}, Total Main Track Papers:{}, Total Workshop Papers:{}".format(a,b,c,d))
+            print()
+            pass
+        
+
+def mainpage():
+    choice=0
+    while choice!=8:
+        print("Who are you? \n 1) Author \n 2) Volunteer\n 3) Organizers \n 4) Tutors \n 5) Attendees \n 6) Workshop Organizers \n 7) Statistics and Ranking \n 8) Exit\n")
+        choice=int(input("Select option: "))
+        if choice==1:
+            authors()
+        elif choice==2:
+            volunteers()
+        elif choice==3:
+            organizers()
+        elif choice==4:
+            tutors()
+        elif choice==5:
+            attendees()
+        elif choice==6:
+            workshop_organizers()
+        if choice==7:
+            stats()
+
+if __name__=="__main__":
+    print("\n\nWelcome to the Conference Manamment System")
+    print("==========================================")
+    result = pyfiglet.figlet_format("SPECTRUM DB",font="slant")
+    print(result)
+    try:
+        print("Trying to connect...")
+        connect = mysql.connector.connect(user='root', password='root123',host='127.0.0.1', database='spectrum')
+        if connect.is_connected:
+            cursor = connect.cursor()
+            print("Connected to the spectrum database")
+            mainpage()
+
+    except mysql.connector.Error as e:
+        print("Error Connecting to database : \n" + str(e))
+    finally:
+        if(connect.is_connected):
+            cursor.close()
+            connect.close()
+            print("Disconnected to the database.")
--- load_data.sql
+++ load_data.sql
@@ -1,84 +1,84 @@
-USE SPECTRUM;
+USE SPECTRUM1;
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/authors.csv' 
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//authors.csv' 
 INTO TABLE Authors FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS (AuthorID,AuthorName,Age,Affiliation,Nationality,Email,Present);
+IGNORE 1 ROWS (AuthorID, AuthorName, Age,Affiliation,Nationality, Email, Present);
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/maintrack_papers.csv'
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//maintrack_papers.csv'
 INTO TABLE MainTrackPapers FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
-IGNORE 1 ROWS (PaperID,Title);
+IGNORE 1 ROWS (PaperID, Title);
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/workshops.csv' 
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//workshops.csv' 
 INTO TABLE Workshops FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS (WorkshopID,WName);
+IGNORE 1 ROWS (WorkshopID, WName);
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/wkshp_papers.csv' 
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//wkshp_papers.csv' 
 INTO TABLE WorkshopPapers FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS (WPaperID,Title,WorkshopID);
+IGNORE 1 ROWS (WPaperID, Title, WorkshopID);
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/organizers.csv' 
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//organizers.csv' 
 INTO TABLE Organizers FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
 IGNORE 1 ROWS (OrganizerID, OrgName, Age, Affiliation, OrgRole, PhoneNumber, Email, Nationality);
 
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/workshop_organizers.csv' 
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//workshop_organizers.csv' 
 INTO TABLE WorkshopOrganizers FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
 IGNORE 1 ROWS (WOID, WOName, Affiliation, Email, @vAuthorID, @vOrganizerID)
 SET
 AuthorID = NULLIF(@vAuthorID, 0),
 OrganizerID = NULLIF(@vOrganizerID, 0);
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/volunteers.csv' 
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//volunteers.csv' 
 INTO TABLE Volunteers FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS (VolunteerID,VolunteerName,Affiliation,Age,Country,Email,PhoneNumber,@vWOID)
+IGNORE 1 ROWS (VolunteerID, VolunteerName, Affiliation, Age, Country, Email, PhoneNumber, @vWOID)
 SET
 WOID = NULLIF(@vWOID, 0);
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/awards.csv' 
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//awards.csv' 
 INTO TABLE Awards FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS (AwardID, AwardName, @vPaperID,@vWPaperID)
+IGNORE 1 ROWS (AwardID, AwardName, @vPaperID, @vWPaperID)
 SET
 PaperID = NULLIF(@vPaperID, 0),
 WPaperID = NULLIF(@vWPaperID, 0);
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/external_attendees.csv' 
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//external_attendees.csv' 
 INTO TABLE ExternalAttendees FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS (AttendeeID,AttendeeName,Age,Affiliation,Email,Present,Country);
+IGNORE 1 ROWS (AttendeeID, AttendeeName, Age, Affiliation, Email, Present, Country);
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/tutorials.csv' 
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//tutorials.csv' 
 INTO TABLE Tutorials FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS (TutorialID,Topic,TutDate, Timing);
+IGNORE 1 ROWS (TutorialID, Topic, TutDate, Timing);
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/tutors.csv'
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//tutors.csv'
 INTO TABLE Tutors FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
-IGNORE 1 ROWS (TutorID,TutorName,Age,Affiliation,Email,Nationality,@vAuthorID,@vTutorialID)
+IGNORE 1 ROWS (TutorID, TutorName, Age, Affiliation, Email, Nationality, @vAuthorID, @vTutorialID)
 SET 
 AuthorID = NULLIF(@vAuthorID, 0),
 TutorialID = NULLIF(@vTutorialID, 0);
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/maintrack_authors.csv' 
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//maintrack_authors.csv' 
 INTO TABLE MainTrackAuthors FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS (AuthorID,	PaperID);
+IGNORE 1 ROWS (AuthorID, PaperID);
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/wkshp_paper_authors.csv' 
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//wkshp_paper_authors.csv' 
 INTO TABLE WkshpPaperAuthors FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS (AuthorID,	WPaperID);
+IGNORE 1 ROWS (AuthorID, WPaperID);
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/volunteers_to_organizers.csv' 
-INTO TABLE MainTrackAuthors FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//volunteers_to_organizers.csv' 
+INTO TABLE VolunteerReportsToOrganizers FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
 IGNORE 1 ROWS (VolunteerID, OrganizerID);
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/authors_attends_tut.csv' 
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//authors_attends_tut.csv' 
 INTO TABLE AuthorAttendsTutorial FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
 IGNORE 1 ROWS (TutorialID, AuthorID, Present);
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/wo_to_wkshp.csv' 
-INTO TABLE AuthorAttendsTutorial FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS (TutorialID, AuthorID, Present);
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//wo_to_wkshp.csv' 
+INTO TABLE WorkOrgOrganizesWorkshop FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
+IGNORE 1 ROWS (WOID, WorkshopID);
 
-LOAD DATA LOCAL INFILE '/Users/lohanis/Desktop/DBMS-project/data/attendees_to_tuts.csv' 
+LOAD DATA LOCAL INFILE 'C://Users//Dell//Desktop//DBMS-project//data//attendees_to_tuts.csv' 
 INTO TABLE AttendeeAttendsTutorial FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS (TutorialID, AttendeeID,Present);
+IGNORE 1 ROWS (TutorialID, AttendeeID, Present);
 
 select * from AttendeeAttendsTutorial;
 select * from AuthorAttendsTutorial;
