127576132936f8e9cdff042168146a89588d87e6
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2021-04-18 17:59:18 +0530

    Updated Indexes

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -1,46 +1,44 @@
-MainTrackAuthors_AuthorID_idx -- 6
-WkshpPaperAuthors_AuthorID_idx -- 5
-MainTrackAuthors_PaperID_idx -- 5
-WkshpPaperAuthors_WPaperID_idx -- 4
-Tutors_TutorialID_idx -- 3
-WorkOrgOrganizesWorkshop_WorkshopID_idx -- 2
-WorkOrgOrganizesWorkshop_WOID_idx -- 2
-Volunteers_WOID_idx -- 2
-VolunteerReportsToOrganizers_VolunteerID_idx -- 2
-VolunteerReportsToOrganizers_OrganizerID_idx -- 2
-WorkshopOrganizers_OrganizerID_idx -- 2
-Awards_PaperID_idx -- 2
-Awards_WPaperID_idx --2
-Authors_AuthorID_Affiliation_idx -- 1 (composite)
-Organizers_OrganizerID_OrgRole_idx -- 1 (composite)
+CREATE INDEX Authors_AuthorID_Affiliation_idx
+ON Authors(AuthorID, Affiliation);
 
+CREATE INDEX WkshpPaperAuthors_AuthorID_idx
+ON WkshpPaperAuthors(AuthorID);
 
+CREATE INDEX WkshpPaperAuthors_WPaperID_idx
+ON WkshpPaperAuthors(WPaperID);
 
+CREATE INDEX Awards_PaperID_idx
+ON Awards(PaperID);
 
-/* CREATE INDEX AuthorName_idx
-ON Authors (AuthorName);
+CREATE INDEX Awards_WPaperID_idx
+ON Awards(WPaperID);
 
-CREATE INDEX AuthorAffiliation_idx
-ON Authors(Affiliation);
+CREATE INDEX VolunteerReportsToOrganizers_VolunteerID_idx
+ON VolunteerReportsToOrganizers(VolunteerID);
 
-CREATE INDEX WkshpPaperAuthors_idx
-ON WkshpPaperAuthors (AuthorID);
+CREATE INDEX VolunteerReportsToOrganizers_OrganizerID_idx
+ON VolunteerReportsToOrganizers(OrganizerID);
 
-CREATE INDEX AwardsPaper_idx
-ON Awards (PaperID);
+CREATE INDEX Volunteers_WOID_idx
+ON Volunteers(WOID);
 
-CREATE INDEX AwardsWkshpPaper_idx
-ON Awards (WPaperID);
+CREATE INDEX AttendeeAttendsTutorial_TutorialID_idx
+ON AttendeeAttendsTutorial(TutorialID);
 
-CREATE INDEX VolunteerIDReportsToOrganizers_idx
-ON VolunteerReportsToOrganizers (VolunteerID);
+CREATE INDEX MainTrackAuthors_PaperID_idx
+ON MainTrackAuthors(PaperID);
 
-CREATE INDEX VolunteerReportsToOrganizerID_idx
-ON VolunteerReportsToOrganizers (OrganizerID);
+CREATE INDEX MainTrackAuthors_AuthorID_idx
+ON MainTrackAuthors(AuthorID);
 
-CREATE INDEX VolunteersName_idx
-ON Volunteers (VolunteerName);
+CREATE INDEX Tutors_TutorialID_idx
+ON Tutors(TutorialID);
 
-CREATE INDEX AttendeeAttendsTutorialID_idx
-ON AttendeeAttendsTutorial (TutorialID);
- */
+CREATE INDEX Organizers_OrganizerID_OrgRole_idx
+ON Organizers(OrgRole);
+
+CREATE INDEX WorkshopOrganizers_OrganizerID_idx
+ON WorkshopOrganizer(OrganizerID);
+
+CREATE INDEX WorkOrgOrganizesWorkshop_WorkshopID_idx
+ON WorkOrgOrganizesWorkshop(WorkshopID);
